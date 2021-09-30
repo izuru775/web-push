457a9afd4a9953e4ad7f54da61995e1e5a699744
izuru775 <izuru775@gmail.com>
2021-09-30 23:12:24 +1000

    client.js added

--- client/client.js
+++ client/client.js
@@ -1,18 +1,48 @@
-const publicVapidKey ='BMBvbaQdTnBIX37FX-3fWhWG8an1z2GmhSDzPQrAJVXwqbwvdfONUyWbR5xqDyui3BUtCISUPsXnzEkPrwbdEWw';
+const publicVapidKey = 'BMBvbaQdTnBIX37FX-3fWhWG8an1z2GmhSDzPQrAJVXwqbwvdfONUyWbR5xqDyui3BUtCISUPsXnzEkPrwbdEWw';
 
 // Check for service worker
-if('serviceWorker' in navigator){
-    send().catch(err=>console.error(err));
+if ('serviceWorker' in navigator) {
+    send().catch(err => console.error(err));
 }
 // Segister SW, Register push, Send push
-async function send(){
+async function send() {
     // Register service worker
-    console.log("registering service worker.....");
-    const register =await navigator.serviceWorker.register('/worker.js',{
-        scope:'/'
+    console.log("Registering service worker.....");
+    const register = await navigator.serviceWorker.register('/worker.js', {
+        scope: '/'
     });
-    console.log('service worker registered...');
+    console.log('Service worker registered...');
 
     // Register push
-    console.log("")
+    console.log("Registering push....");
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    })
 }
+    console.log("Push registered...");
+
+    // Push notification
+    console.log("Sending Push...")
+    await fetch('/subscribe',{
+        method:"POST",
+        body:JSON.stringify(subscription),
+        headers:{
+            'content-type':'application/json'
+        }
+    });
+    console.log("Push sent...")
+function urlBase64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, "+")
+      .replace(/_/g, "/");
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
