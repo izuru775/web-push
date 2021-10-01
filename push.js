29b3b354d2a36073842ab8f0c563774cce5139cf
izuru775 <izuru775@gmail.com>
2021-10-01 16:53:16 +1000

    sub variable renewed

--- push.js
+++ push.js
@@ -9,12 +9,12 @@ push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.priv
 
 // Usually the list of people in the db where we want to send the notification
 let sub = {
-    endpoint:"https://fcm.googleapis.com/fcm/send/e5YAdYO5F30:APA91bGieNXYQC7cGtPUGl8rz1Wi9y-j0O5DjyBY2QfDmnh14Ss5qL-UknqCO9MN2whmqIVaZclzmS8jSnf7YBxA7EpsTyfWjOhO5WKF1Lg8C9AQdnlK1TfSX6KbmPlz9R06Hi_o5qnO",
-    expirationTime:null,
-    keys:{
-        p256dh:"BNwqk11Y61cwCMWsc-8ZZ7ATXCfBWmc2NuwV-7nyic3sFXkyyEeuM5XRPN92Vdg_FsHijxBLVIfIVBAVKjdpZXo",
-        auth:"fI4tolPZeqyVBrTIJ2ZwWw"
+    endpoint: "https://fcm.googleapis.com/fcm/send/e5YAdYO5F30:APA91bGieNXYQC7cGtPUGl8rz1Wi9y-j0O5DjyBY2QfDmnh14Ss5qL-UknqCO9MN2whmqIVaZclzmS8jSnf7YBxA7EpsTyfWjOhO5WKF1Lg8C9AQdnlK1TfSX6KbmPlz9R06Hi_o5qnO",
+    expirationTime: null,
+    keys: {
+        p256dh: "BNwqk11Y61cwCMWsc-8ZZ7ATXCfBWmc2NuwV-7nyic3sFXkyyEeuM5XRPN92Vdg_FsHijxBLVIfIVBAVKjdpZXo",
+        auth: "fI4tolPZeqyVBrTIJ2ZwWw"
     }
 }
 
-push.sendNotification(sub,'test message');
+push.sendNotification(sub, 'test message');
