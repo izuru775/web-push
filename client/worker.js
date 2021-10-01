99b3bbedb88ea3622ef6e65549411db291db1150
izuru775 <izuru775@gmail.com>
2021-10-01 12:25:21 +1000

    No changes

--- client/worker.js
+++ client/worker.js
@@ -1,10 +1,11 @@
 console.log("Service worker loaded...");
 
-self.addEventListener('psuh',e=>{
+self.addEventListener('push', e => {
     const data = e.data.json();
     console.log("Push Received...");
-    self.registration.showNotification(data.title,{
-        body:'Notified by Isuru Adikaram',
-        icon:'https://img.icons8.com/material-outlined/24/000000/link--v1.png'
+
+    self.registration.showNotification(data.title, {
+        body: "Notified by Isuru Adikaram",
+        icon: "https://img.icons8.com/material-outlined/24/000000/link--v1.png"
     });
-})
+});
