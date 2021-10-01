849e2f50b91367f31775f7298447e403cc69669f
izuru775 <izuru775@gmail.com>
2021-10-01 16:37:01 +1000

    Minor changes

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
 self.addEventListener('push', function (e) {
-    var body;
-
-    if (e.data) {
-        body = e.data.text();
-    } else {
-        body = 'Push message no payload';
-    }
 
     var options = {
         body: body,
