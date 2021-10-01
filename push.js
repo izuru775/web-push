bce16cc73ff1b60f72db7319c16bdd80b060009d
izuru775 <izuru775@gmail.com>
2021-10-01 16:31:42 +1000

    Fix

--- push.js
+++ push.js
@@ -5,9 +5,16 @@ let vapidKeys = {
     privateKey: 'xjR9GuSkq_iwPiwWhbZ3N-Z__A7_oyKCLHnjoZlES2w'
 }
 
-push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
 // Usually the list of people in the db where we want to send the notification
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/e5YAdYO5F30:APA91bGieNXYQC7cGtPUGl8rz1Wi9y-j0O5DjyBY2QfDmnh14Ss5qL-UknqCO9MN2whmqIVaZclzmS8jSnf7YBxA7EpsTyfWjOhO5WKF1Lg8C9AQdnlK1TfSX6KbmPlz9R06Hi_o5qnO",
+    expirationTime:null,
+    keys:{
+        p256dh:"BNwqk11Y61cwCMWsc-8ZZ7ATXCfBWmc2NuwV-7nyic3sFXkyyEeuM5XRPN92Vdg_FsHijxBLVIfIVBAVKjdpZXo",
+        auth:"fI4tolPZeqyVBrTIJ2ZwWw"
+    }
+}
 
-push.sendNotification(sub,'test message')
+push.sendNotification(sub,'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,32 @@
-self.addEventListener('push',()=>{
-    self.registration.sendNotification('test message',{})
-})
+self.addEventListener('push', function (e) {
+    var body;
+
+    if (e.data) {
+        body = e.data.text();
+    } else {
+        body = 'Push message no payload';
+    }
+
+    var options = {
+        body: body,
+        icon: 'images/notification-flat.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        },
+        actions: [
+            {
+                action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close', title: 'I don\'t want any of this',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
